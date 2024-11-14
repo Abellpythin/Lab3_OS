6b239ae7b39e8441a89499e9618eb328ef6b0b15
Abellpythin <aaliabell18@gmail.com>
2024-11-14 09:39:43 -0500

    update

--- lab3.c
+++ lab3.c
@@ -11,7 +11,24 @@ int** read_board_from_file(char* filename){
     int** board = NULL;
 
     // replace this comment with your code
+    for (int i = 0; i < ROW_SIZE; i++) {
+        board[i] = (int*)malloc(sizeof(int) * COL_SIZE);  // Allocate memory for columns
+        if (!board[i]) {
+            perror("Error allocating memory for board columns");
+            fclose(fp);
+            return NULL;
+        }
 
+        // Read each number from file
+        for (int j = 0; j < COL_SIZE; j++) {
+            if (fscanf(fp, "%d,", &board[i][j]) != 1) {
+                perror("Error reading data from file");
+                fclose(fp);
+                return NULL;
+            }
+        }
+    }
+    fclose(fp);
     return board;
 }
 
@@ -22,5 +39,6 @@ int is_board_valid(){
     param_struct* parameter;
     
     // replace this comment with your code
+    
     
 }
